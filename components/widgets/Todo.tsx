8f43810fce75801da0e6193108de814525d4efc9
Ethan Engm <ethaneng01@hotmail.com>
2023-06-10 22:34:38 +0800

    can delete todos and check them off

--- components/widgets/Todo.tsx
+++ components/widgets/Todo.tsx
@@ -25,24 +25,48 @@ export default function Todo({x, y}: Props) {
         setNewTodo("")
     }
 
-    function handleCheck() {
-    
+    function handleCheck(e: React.ChangeEvent<HTMLInputElement>) {
+        setTodos(todos.map((todo) => {
+            if (todo.id === e.target.id) {
+                return {...todo, completed: !todo.completed}
+            } else {
+                return todo
+            }
+        }))
+    }
+
+    function handleDelete(index: number) {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
+
+    function handleEnter(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            handleAddTodo()
+        }
     }
 
   return (
     <Widget x={x} y={y} size="tall">
-        <div className='w-full h-[50px] flex flex-col p-2'>
-            <label>New Todo:</label>
-            <div className='flex flex-row items-center gap-1'>
-                <input className=' bg-white border-2 w-full' value={newTodo} onChange={(e) => (setNewTodo(e.target.value))} />
-                <button onClick={handleAddTodo} className='bg-green-400 px-1'>+</button>
+        <div className='w-full h-[50px] flex flex-col p-2 gap-2'>
+            <div>
+                <label>New Todo:</label>
+                <div className='flex flex-row items-center gap-1'>
+                    <input type='text' className=' bg-white border-2 w-full' value={newTodo} onKeyDown={handleEnter} onChange={(e) => (setNewTodo(e.target.value))} />
+                    <button onClick={handleAddTodo} className='bg-green-400 px-1'>+</button>
+                </div>
             </div>
-            {todos.map((todo) => (
-                <li key={todo.id} className='list-none flex justify-start items-center gap-1'>
-                    <input type='checkbox' checked={todo.completed} />
-                    <span className={`${todo.completed ? 'line-through' : ''}`}>{todo.name}</span>
-                </li>
-            ))}
+
+            <div className='flex flex-col gap-2'>
+                {todos.map((todo, index) => (
+                    <li key={todo.id} className='list-none flex justify-start items-center gap-1'>
+                        <input id={todo.id} onChange={handleCheck} type='checkbox' checked={todo.completed} className='cursor-pointer' />
+                        <label htmlFor={todo.id} className={`leading-tight text-sm ${todo.completed ? 'line-through' : ''}`}>{todo.name}</label>
+                        <button onClick={() => handleDelete(index)} className='text-red-400'>X</button>
+                    </li>
+                ))}
+            </div>
         </div>
     </Widget>
   )
