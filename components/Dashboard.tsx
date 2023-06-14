f2161a0167a5284cd94e81670ea0345b84cafa8b
Ethan Engm <ethaneng01@hotmail.com>
2023-06-14 09:02:44 +0800

    Added menu UI

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Clock from './widgets/Clock'
 import QOTD from './widgets/QOTD'
 import Todo from './widgets/Todo'
+import Menu from './Menu'
 
 function Dashboard() {
 
   return (
     <div className='dashboard relative flex-grow'>
+        <Menu />
         <Clock x={100} y={50} />
         <QOTD x={200} y={100} />
         <Todo x={300} y={100} />
---
+++ components/Menu.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <ul className='fixed bottom-4 right-4 flex flex-col-reverse items-center gap-4 group'>
+        <li className=''>
+            <button className='rounded outline p-2'>NEW</button>
+        </li>
+        <li className='hidden group-hover:'>
+            <button className='rounded outline p-2'>1</button>
+        </li>
+        <li className='hidden group-hover:visible'>
+            <button className='rounded outline p-2'>2</button>
+        </li>
+        <li className='hidden group-hover:visible'>
+            <button className='rounded outline p-2'>3</button>
+        </li>
+    </ul>
+  )
+}
