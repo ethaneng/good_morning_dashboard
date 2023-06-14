58589c0ad0953ca8e1de4a477c970bff661b6876
Ethan Engm <ethaneng01@hotmail.com>
2023-06-14 13:32:08 +0800

    Added state for dashboard widgets

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,17 +1,43 @@
-import React from 'react'
+"use client"
+
+import {useEffect, useState} from 'react'
 import Clock from './widgets/Clock'
 import QOTD from './widgets/QOTD'
 import Todo from './widgets/Todo'
 import Menu from './Menu'
 
+type DashboardWidget = {
+  id: string,
+  x: number,
+  y: number,
+  type: 'Clock' | 'QOTD' | 'Todo'
+}
+
 function Dashboard() {
+
+  const [widgets, setWidgets] = useState<DashboardWidget[]>([
+    {
+      id: crypto.randomUUID(),
+      x: 100,
+      y: 50,
+      type: 'Clock'
+    },
+    {
+      id: crypto.randomUUID(),
+      x: 300,
+      y: 50,
+      type: 'QOTD'
+    }
+  ])
 
   return (
     <div className='dashboard relative flex-grow'>
         <Menu />
-        <Clock x={100} y={50} />
-        <QOTD x={200} y={100} />
-        <Todo x={300} y={100} />
+        {widgets.map((widget) => {
+          if (widget.type === 'Clock') return <Clock key={widget.id} x={widget.x} y={widget.y} />
+          if (widget.type === 'QOTD') return <QOTD key={widget.id} x={widget.x} y={widget.y} />
+          if (widget.type === 'Todo') return <Todo key={widget.id} x={widget.x} y={widget.y} />
+        })}
     </div>
   )
 }
